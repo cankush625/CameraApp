8c92f106907c47e045427921aa434e17116e2adb
cankush625 <cankush625@gmail.com>
2020-03-28 22:52:15 +0530

    Initial commit
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ CameraApp.py
@@ -0,0 +1,21 @@
+import cv2
+
+# Start capturing webcam
+cap = cv2.VideoCapture(0)
+
+# Capturing photo for each milisecond and runing for each mili second to for a stream video
+while True:
+    # Capturing photo
+    status, photo = cap.read()
+    # Showing photo
+    cv2.imshow('ankush.jpg', photo)
+    # Capturing photo for each milisecond
+    cv2.waitKey(1)
+    # Esc key has ascii value 27 hence to close the app window we need to press esc key
+    if cv2.waitKey(1) == 27:
+        break
+
+# Destroying the output window
+cv2.destroyAllWindows()
+# Closing the webcam
+cap.release()
